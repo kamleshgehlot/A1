844ff56913c06d587f10752fe29bb2162357eae0
mukesh purohit <mpurohit88@gmail.com>
2019-07-12 21:50:48 +0530

    Email changes

--- server/lib/mailtransporter.js
+++ server/lib/mailtransporter.js
@@ -1,16 +1,17 @@
 const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
 
-const mailAccountUser = 'sktanwar.2020@gmail.com'
-const mailAccountPassword = '829044701486'
+const mailAccountUser = 'admin@rentronics.saimrc.com'
+const mailAccountPassword = 'cxqD@Yir2'
 
-var trans = nodemailer.createTransport(smtpTransport({
-  service: 'gmail.com',
+const trans = nodemailer.createTransport(smtpTransport({
+  service: 'rentronics.saimrc.com',
   tls: { rejectUnauthorized: false },
   auth: {
     user: mailAccountUser,
     pass: mailAccountPassword
   }
 }))
+
 
 module.exports = { trans: trans };
