55c3947e12fd19693c4e5c15859e938f54ae6c4e
mukesh purohit <mpurohit88@gmail.com>
2019-07-14 08:31:05 +0530

    rentronics

--- server/lib/databaseMySQLNew.js
+++ server/lib/databaseMySQLNew.js
@@ -1,1 +1,1 @@
-module.exports = { 'prod': 'rentrodev_test', getFullName: function (dbName, userName) { return dbName + "_" + userName } };
+module.exports = { 'prod': 'rentronics', getFullName: function (dbName, userName) { return dbName + "_" + userName } };
