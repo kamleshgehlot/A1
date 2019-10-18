0c879689be70f9c9a3114b8d33cafbe9b7d6ba44
mpurohit88@gmail.com <mpurohit88@gmail.com>
2019-10-18 18:54:07 +0530

    db

--- server/db/18102019_AlterBudgetAndFlexOrder.sql
+++ server/db/18102019_AlterBudgetAndFlexOrder.sql
@@ -1,2 +1,3 @@
 ALTER TABLE budget ADD paid_day VARCHAR(20) NULL AFTER afford_amt, ADD debited_day VARCHAR(20) NULL AFTER paid_day;
 ALTER TABLE flex_order DROP duration;
+ALTER TABLE enquiry ADD reason_to_delete VARCHAR(500) NULL AFTER converted_to;
