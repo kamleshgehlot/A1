c845ba06918afb2e9327edc32603305c12a6e3cf
sktanwar2014 <sktanwar.2014@gmail.com>
2020-03-07 15:20:08 +0530

    push

--- server/db/ezidebit_pay_params.sql
+++ server/db/ezidebit_pay_params.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2019 at 02:09 PM
+-- Generation Time: Mar 07, 2020 at 08:15 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.1.27
 
@@ -30,31 +30,33 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `ezidebit_pay_params` (
   `id` int(11) NOT NULL,
-  `payment_type_id` int(11) DEFAULT NULL,
-  `payment_type` varchar(20) DEFAULT NULL,
-  `payment_type_is_active` tinyint(1) DEFAULT NULL,
-  `payment_method_id` int(11) DEFAULT NULL,
-  `payment_method` varchar(50) DEFAULT NULL,
-  `payment_method_is_active` tinyint(1) DEFAULT NULL,
-  `payment_source_id` int(11) DEFAULT NULL,
-  `payment_source` varchar(20) DEFAULT NULL,
-  `payment_source_is_active` tinyint(1) DEFAULT NULL,
-  `date_field_id` int(11) DEFAULT NULL,
-  `date_field` varchar(20) DEFAULT NULL,
-  `date_field_is_active` tinyint(1) DEFAULT NULL
+  `type` varchar(100) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `is_active` int(1) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `ezidebit_pay_params`
 --
 
-INSERT INTO `ezidebit_pay_params` (`id`, `payment_type_id`, `payment_type`, `payment_type_is_active`, `payment_method_id`, `payment_method`, `payment_method_is_active`, `payment_source_id`, `payment_source`, `payment_source_is_active`, `date_field_id`, `date_field`, `date_field_is_active`) VALUES
-(1, 1, 'ALL', 1, 1, 'ALL', 1, 1, 'ALL', 1, 1, 'PAYMENT', 1),
-(2, 2, 'PENDING', 1, 2, 'CR', 1, 2, 'SCHEDULED', 1, 2, 'SETTLEMENT', 1),
-(3, 3, 'FAILED', 1, 3, 'DR', 1, 3, 'WEB', 1, NULL, NULL, NULL),
-(4, 4, 'SUCCESSFUL', 1, NULL, NULL, NULL, 4, 'PHONE', 1, NULL, NULL, NULL),
-(5, NULL, NULL, NULL, NULL, NULL, NULL, 5, 'BPAY', 1, NULL, NULL, NULL),
-(6, NULL, NULL, NULL, NULL, NULL, NULL, 6, 'ADJ', 1, NULL, NULL, NULL);
+INSERT INTO `ezidebit_pay_params` (`id`, `type`, `value`, `is_active`, `created_by`, `created_at`) VALUES
+(1, 'paymentType', 'ALL', 1, 1, '2020-03-07 06:12:46'),
+(2, 'paymentType', 'PENDING', 1, 1, '2020-03-07 06:12:46'),
+(3, 'paymentType', 'FAILED', 1, 1, '2020-03-07 06:12:46'),
+(4, 'paymentType', 'SUCCESSFUL', 1, 1, '2020-03-07 06:12:46'),
+(5, 'paymentMethod', 'ALL', 1, 1, '2020-03-07 06:12:46'),
+(6, 'paymentMethod', 'BANK ACCOUNT', 1, 1, '2020-03-07 06:12:46'),
+(7, 'paymentMethod', 'CREDIT CARD', 1, 1, '2020-03-07 06:12:46'),
+(8, 'paymentSource', 'ALL', 1, 1, '2020-03-07 06:12:46'),
+(9, 'paymentSource', 'SCHEDULED', 1, 1, '2020-03-07 06:12:46'),
+(10, 'paymentSource', 'WEB', 1, 1, '2020-03-07 06:12:46'),
+(11, 'paymentSource', 'PHONE', 1, 1, '2020-03-07 06:12:46'),
+(12, 'paymentSource', 'BPAY', 1, 1, '2020-03-07 06:12:46'),
+(13, 'paymentSource', 'ADJ', 1, 1, '2020-03-07 06:12:46'),
+(14, 'dateField', 'PAYMENT', 1, 1, '2020-03-07 06:12:46'),
+(15, 'dateField', 'SETTLEMENT', 1, 1, '2020-03-07 06:12:46');
 
 --
 -- Indexes for dumped tables
@@ -74,7 +76,7 @@ ALTER TABLE `ezidebit_pay_params`
 -- AUTO_INCREMENT for table `ezidebit_pay_params`
 --
 ALTER TABLE `ezidebit_pay_params`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
